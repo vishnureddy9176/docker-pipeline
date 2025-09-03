5656a05f2681acb0c90a805772001c47faa4e8cf
root <root@ip-172-31-32-179.ap-south-1.compute.internal>
2025-09-03 09:37:39 +0000

    files for docker pipeline

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM nginx
+MAINTAINER name vishnu
+LABEL this is first ever pipeline ffor docker
+copy . /usr/share/nginx/html
+
---
+++ jenkins.sh
@@ -0,0 +1,16 @@
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo yum install java-17-amazon-corretto -y
+sudo yum install jenkins git -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo mkdir -p /var/tmp_disk
+sudo chmod 1777 /var/tmp_disk
+sudo mount --bind /var/tmp_disk /tmp
+echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
+sudo systemctl mask tmp.mount
+df -h /tmp
+sudo systemctl restart jenkins
